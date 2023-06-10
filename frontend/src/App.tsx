6d8534e747af14b8974f4e8ae5a647a7f28ea6d7
Geuni <dlrmsgnl620@naver.com>
2023-06-11 00:53:47 +0900

    Chore: add settings for lint
    - set ts-config, eslint, prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import {useState} from "react";
-import reactLogo from "src/assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from 'react';
+import reactLogo from 'src/assets/react.svg';
+import viteLogo from '/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,10 +8,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
