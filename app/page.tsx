8a27a7901c737095b8adce5caec397421d50e88d
Geuni <dlrmsgnl620@naver.com>
2023-11-26 01:41:31 +0900

    Feat: Nav 내 카카오 로그인 버튼 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import { KakaoLoginButton } from 'components/KakaoLoginButton';
-
 export default function Home() {
-  return <KakaoLoginButton />;
+  return (
+    <div className="h-screen">
+      <div className="bg-red-500 h-full">임시 색상</div>
+    </div>
+  );
 }
