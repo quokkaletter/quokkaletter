7c4a3b91006c74e80ee21cc8d429128d276389c1
hyodduru <hyodduru@hyottuui-MacBookAir.local>
2023-12-04 21:08:54 +0900

    fix: 불필요한 콘솔로그 제거

--- components/KakaoLoginButton.tsx
+++ components/KakaoLoginButton.tsx
@@ -6,8 +6,6 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 export const KakaoLoginButton = () => {
   const { data: session } = useSession();
 
-  console.log('session', session);
-
   if (session) {
     return (
       <>
