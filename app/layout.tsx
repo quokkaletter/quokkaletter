0722884e8448d8fef7fdeceab54c78843840c75e
Geuni <dlrmsgnl620@naver.com>
2023-12-17 13:55:24 +0900

    Fix: change max width 600px

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <div id="modal" />
         <AuthSession>
           <QueryProvider>
-            <main className="mx-auto max-w-[500px] h-screen">
+            <main className="mx-auto max-w-[600px] h-screen">
               {children}
               <div className="absolute right-0 bottom-0 h-screen w-[calc(50%-250px)] bg-white" />
             </main>
