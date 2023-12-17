97053145fb8e1de926bcbc189e349608b001ffa0
hyodduru <hyodduru@hyottuui-MacBookAir.local>
2023-12-17 21:17:04 +0900

    feat: nav 외부 창 클릭 시 닫히도록 처리 - 로고 클릭 시 본 화면으로 이동 시킴

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,7 +2,7 @@
 
 import { TextAlignJustifyIcon } from '@radix-ui/react-icons';
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Nav } from 'components/nav';
 
 export const Header: React.FC = () => {
@@ -17,20 +17,43 @@ export const Header: React.FC = () => {
     closed: { x: '100%' },
   };
 
+  const navRef = useRef(null);
+  const hamburgerRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: any) => {
+      if (
+        navRef.current &&
+        !navRef.current.contains(event.target) &&
+        !hamburgerRef.current.contains(event.target)
+      ) {
+        setIsNavVisible(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [isNavVisible]);
+
   return (
     <header className="bg-[#588251]/50 text-white flex justify-between p-3 relative">
-      <p>quokka letter</p>
+      <a href="/">quokka letter</a>
       <TextAlignJustifyIcon
         onClick={toggleNav}
         className="w-[20px] h-[20px] cursor-pointer"
+        ref={hamburgerRef}
       />
-
       <motion.nav
         initial="closed"
         animate={isNavVisible ? 'opened' : 'closed'}
         exit="closed"
         variants={navVariants}
         className="bg-[#588251]/50 w-[50%] absolute right-0 top-[48px] h-screen"
+        transition={{ duration: 0.3 }}
+        ref={navRef}
       >
         <Nav />
       </motion.nav>
