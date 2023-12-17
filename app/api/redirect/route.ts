2d3b1f5362fb2d1dfe9a135bbbb398cefb805d32
Hansol <losnah24@gmail.com>
2023-12-17 20:17:40 +0900

    fix: redirect api 수정

--- app/api/redirect/route.ts
+++ app/api/redirect/route.ts
@@ -1,15 +1,18 @@
 import { firebaseApp } from 'utils/firebaseApp';
 import { NextResponse } from 'next/server';
 
-export const POST = async (req: Request) => {
+export async function POST(req: Request) {
   const { userId } = await req.json();
   const userDoc = await firebaseApp.collection('users').doc(userId).get();
   const user = userDoc.data();
   if (!user.nickname) {
-    console.log(
-      `this user does not have nickname. redirect to ${process.env.NEXTAUTH_URL}/join`,
-    );
-    return new NextResponse('/join', { status: 200 });
+    return NextResponse.json({
+      message: `this user: ${userId} does not have nickname`,
+      redirect: '/join',
+    });
   }
-  return new NextResponse(`/dashboard/${userId}`, { status: 200 });
-};
+  return NextResponse.json({
+    message: 'success',
+    redirect: '/dashboard',
+  });
+}
