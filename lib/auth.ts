4c6769489dcc11843c71ccc9f7a7670a642939e1
Hansol Jeong <losnah24@gmail.com>
2023-12-17 15:00:50 +0900

    [Feat] firebase setting (#9)
    * adapter 추가
    
    * env 샘플 추가

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,5 @@
+import { FirestoreAdapter } from '@auth/firebase-adapter';
+import { cert } from 'firebase-admin/app';
 import type { NextAuthOptions } from 'next-auth';
 import KakaoProvider from 'next-auth/providers/kakao';
 
@@ -20,4 +22,11 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+  }),
 };
