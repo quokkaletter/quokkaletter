f003db06f51e50c821ce6257c2b8dfce0455cbee
Lee Keun Hwee <56650238+Geuni620@users.noreply.github.com>
2023-12-17 20:30:50 +0900

    setup: deploy (#19)

--- app/api/redirect/route.ts
+++ app/api/redirect/route.ts
@@ -5,12 +5,14 @@ export async function POST(req: Request) {
   const { userId } = await req.json();
   const userDoc = await firebaseApp.collection('users').doc(userId).get();
   const user = userDoc.data();
+
   if (!user.nickname) {
     return NextResponse.json({
       message: `this user: ${userId} does not have nickname`,
       redirect: '/join',
     });
   }
+
   return NextResponse.json({
     message: 'success',
     redirect: `/dashboard/${userId}`,
