8e38826c6c9b67f7e757530799abd37b65284428
hyodduru <hyodduru@hyottuui-MacBookAir.local>
2023-12-20 23:07:05 +0900

    feat: 쿼카레터 편지 쓰는 api 연동 - 쿼카레터 편지 쓰기 버튼 본인 페이지에서는 노출하지 않도록 처리

--- components/WriteLetterButton.tsx
+++ components/WriteLetterButton.tsx
@@ -3,9 +3,20 @@
 import { Button } from '@/components/ui/button';
 import { useModal } from 'hooks/useModal';
 import { WriteLetterModal } from './WriteLetterModal';
+import { useSession } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
+import { useGetNicknameQuery } from '@hooks/useGetNicknameQuery';
 
 export const WriteLetterButton = () => {
+  const { data: { user } = {} } = useSession();
+  const pathname = usePathname();
+  const recipientId = pathname.match(/\/dashboard\/([a-zA-Z0-9]+)/)[1];
   const { closeModal, setIsModalVisible, isModalVisible } = useModal();
+  const { nickname } = useGetNicknameQuery({ userId: user?.id });
+
+  if (user?.id === recipientId) {
+    return <div style={{ color: 'white' }}>{nickname.data}의 쿼카레터</div>;
+  }
 
   return (
     <>
