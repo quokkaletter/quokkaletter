0c787f2261e7e268559fe8155892bb804335b186
Hansol <losnah24@gmail.com>
2023-12-22 22:02:33 +0900

    Feat: 글 작성 API에서 수신자에게 보여질 닉네임을 추가

--- app/api/letter/route.ts
+++ app/api/letter/route.ts
@@ -2,17 +2,21 @@ import { firebaseApp } from 'utils/firebaseApp';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  const { userId, recipientId, contents } = await req.json();
+  const { userId, recipientId, contents, anonymousNickname } = await req.json();
   await firebaseApp.collection('letters').add({
     writerId: userId,
     recipientId,
     contents,
+    anonymousNickname,
   });
 
-  return NextResponse.json({
-    message: 'success',
-    writerId: userId,
-    recipientId,
-    contents,
-  });
+  return NextResponse.json(
+    {
+      writerId: userId,
+      recipientId,
+      contents,
+      anonymousNickname,
+    },
+    { status: 201, statusText: 'success' },
+  );
 }
