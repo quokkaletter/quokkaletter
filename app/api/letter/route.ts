0f307f6ec0b0429264bb8472f861386d1de84a3f
Hansol <losnah24@gmail.com>
2023-12-27 23:47:42 +0900

    :bricks: 레터 생성 시 isVisible 필드를 추가

--- app/api/letter/route.ts
+++ app/api/letter/route.ts
@@ -10,20 +10,15 @@ export async function POST(req: Request) {
     throw new Error(`Cannot find user. userId was <<${userId}>>`);
   }
 
-  await firebaseApp.collection('letters').add({
+  const addedLetterRef = await firebaseApp.collection('letters').add({
     writerId: userId,
     recipientId,
     contents,
     anonymousNickname,
+    isVisible: false,
   });
+  const addedLetterSnapshot = await addedLetterRef.get();
+  const addedLetter = addedLetterSnapshot.data();
 
-  return NextResponse.json(
-    {
-      writerId: userId,
-      recipientId,
-      contents,
-      anonymousNickname,
-    },
-    { status: 201, statusText: 'success' },
-  );
+  return NextResponse.json(addedLetter, { status: 201, statusText: 'success' });
 }
