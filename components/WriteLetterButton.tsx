0dd8a74df19d9f21e316e15562dc706e4ec91403
hyodduru <hyodduru@hyottuui-MacBookAir.local>
2023-12-29 01:46:09 +0900

    feat: 배경 이미지 적용

--- components/WriteLetterButton.tsx
+++ components/WriteLetterButton.tsx
@@ -5,7 +5,6 @@ import { useModal } from 'hooks/useModal';
 import { WriteLetterModal } from './WriteLetterModal';
 import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
-import { useGetNicknameQuery } from '@hooks/useGetNicknameQuery';
 
 export const WriteLetterButton = () => {
   const { data: session } = useSession();
@@ -13,16 +12,16 @@ export const WriteLetterButton = () => {
   const pathname = usePathname();
   const recipientId = pathname.match(/\/dashboard\/([a-zA-Z0-9]+)/)[1];
   const { closeModal, setIsModalVisible, isModalVisible } = useModal();
-  const { nickname } = useGetNicknameQuery({ userId });
 
   if (userId === recipientId) {
-    return <div style={{ color: 'white' }}>{nickname.data}의 쿼카레터</div>;
+    return null;
   }
 
   return (
     <>
+      {/* TODO: 배경 이미지 교체 이후 위치 조정 필요 */}
       <Button
-        className="bg-[#D9D9D9] text-primary py-2 px-4"
+        className="absolute top-20 left-1/2 transform -translate-x-1/2 bg-[#D9D9D9] text-primary py-2 px-4"
         onClick={() => setIsModalVisible(true)}
       >
         쿼카레터 쓰러가기
