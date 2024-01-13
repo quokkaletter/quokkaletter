8e45f69693139de00865f91cffd9fc2036606105
Hansol Jeong <losnah24@gmail.com>
2024-01-13 15:37:49 +0900

    [Feat] next-swagger 추가 (#40)
    * feat: next-swagger 추가
    
    * feat: 레터 작성, 닉네임 조회 및 등록 swagger 문서 추가
    
    * fix: 레터 작성 api 스펙 수정
    
    * feat: 내가 쓴 레터 조회, 내가 받은 레터 조회 api 스펙 문서 추가

--- app/api/letter/route.ts
+++ app/api/letter/route.ts
@@ -1,6 +1,72 @@
 import { firebaseApp } from 'utils/firebaseApp';
 import { NextResponse } from 'next/server';
 
+/**
+ * @swagger
+ * /api/letter:
+ *   post:
+ *     summary: "레터 작성"
+ *     description: "레터를 생성합니다."
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: string
+ *                 default: "T6AJ38Eupck8uhauf3hk"
+ *                 description: "작성자의 userId 입니다."
+ *               recipientId:
+ *                 type: string
+ *                 default: "jTjtGoiVhl8KnRK9IYwz"
+ *                 description: "수신자의 userId 입니다."
+ *               contents:
+ *                 type: string
+ *                 default: "레터 내용입니다. 테스트"
+ *                 description: "레터 내용입니다."
+ *               anonymousNickname:
+ *                 type: string
+ *                 default: "수신자에게보여질닉네임테스트"
+ *                 description: "수신자에게 보여질 닉네임입니다. 기존의 유저 닉네임과는 무관합니다."
+ *     responses:
+ *       201:
+ *         description: "레터 생성 성공. 생성된 레터 정보를 반환합니다."
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 contents:
+ *                   type: string
+ *                 recipientId:
+ *                   type: string
+ *                 isVisible:
+ *                   type: boolean
+ *                   default: false
+ *                 writerId:
+ *                   type: string
+ *                 anonymousNickname:
+ *                   type: string
+ *                 createdAt:
+ *                   type: object
+ *                   properties:
+ *                     _seconds:
+ *                       type: number
+ *                     _nanoseconds:
+ *                       type: number
+ *                 updatedAt:
+ *                   type: object
+ *                   properties:
+ *                     _seconds:
+ *                       type: number
+ *                     _nanoseconds:
+ *                       type: number
+ *       400:
+ *         description: "클라이언트 측이 잘못된 요청을 보냈습니다."
+ *       500:
+ *         description: "서버에서 내부 오류가 발생했습니다."
+ */
 export async function POST(req: Request) {
   const { userId, recipientId, contents, anonymousNickname } = await req.json();
 
@@ -10,12 +76,15 @@ export async function POST(req: Request) {
     throw new Error(`Cannot find user. userId was <<${userId}>>`);
   }
 
+  const operatedAt = new Date();
   const addedLetterRef = await firebaseApp.collection('letters').add({
     writerId: userId,
     recipientId,
     contents,
     anonymousNickname,
     isVisible: false,
+    createdAt: operatedAt,
+    updatedAt: operatedAt,
   });
   const addedLetterSnapshot = await addedLetterRef.get();
   const addedLetter = addedLetterSnapshot.data();
--- app/api/nickname/route.ts
+++ app/api/nickname/route.ts
@@ -1,6 +1,37 @@
 import { firebaseApp } from 'utils/firebaseApp';
 import { NextResponse } from 'next/server';
 
+/**
+ * @swagger
+ * /api/nickname:
+ *   post:
+ *     summary: "유저 닉네임 등록"
+ *     description: "유저의 닉네임을 등록합니다."
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: string
+ *               nickname:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: "닉네임 등록 성공"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 nickname:
+ *                   type: string
+ *       400:
+ *         description: "클라이언트 측이 잘못된 요청을 보냈습니다."
+ *       500:
+ *         description: "서버에서 내부 오류가 발생했습니다."
+ */
 export async function POST(req: Request) {
   const { userId, nickname } = await req.json();
   await firebaseApp.collection('users').doc(userId).update({
