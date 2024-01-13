525b15fd009b5e544ca9e5b9111460447d434311
Hansol <losnah24@gmail.com>
2024-01-13 14:42:10 +0900

    feat: 레터 작성, 닉네임 조회 및 등록 swagger 문서 추가

--- app/api/nickname/[userId]/route.ts
+++ app/api/nickname/[userId]/route.ts
@@ -1,6 +1,33 @@
 import { NextResponse } from 'next/server';
 import { firebaseApp } from 'utils/firebaseApp';
 
+/**
+ * @swagger
+ * /api/nickname/{userId}:
+ *   get:
+ *     summary: "유저 닉네임 조회"
+ *     description: "유저의 닉네임 정보를 조회합니다."
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: "닉네임 조회 성공"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 nickname:
+ *                   type: string
+ *       400:
+ *         description: "클라이언트 측이 잘못된 요청을 보냈습니다."
+ *       500:
+ *         description: "서버에서 내부 오류가 발생했습니다."
+ */
 export async function GET(
   _: unknown,
   { params }: { params: { userId: string } },
