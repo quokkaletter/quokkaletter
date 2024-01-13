5e8a14056e4dc1e747fb74311ad6923f8da1ef0d
Hansol <losnah24@gmail.com>
2024-01-13 15:36:25 +0900

    feat: 내가 쓴 레터 조회, 내가 받은 레터 조회 api 스펙 문서 추가

--- app/api/letter/[userId]/sent/route.ts
+++ app/api/letter/[userId]/sent/route.ts
@@ -1,6 +1,61 @@
 import { NextResponse } from 'next/server';
 import { firebaseApp } from 'utils/firebaseApp';
 
+/**
+ * @swagger
+ * /api/letter/{userId}/sent:
+ *   get:
+ *     summary: "내가 쓴 레터 조회"
+ *     description: "내가 쓴 레터를 조회합니다."
+ *     parameters:
+ *       - name: userId
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: "내가 쓴 레터 조회 성공"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 letters:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       contents:
+ *                         type: string
+ *                       recipientId:
+ *                         type: string
+ *                       isVisible:
+ *                         type: boolean
+ *                         default: false
+ *                       writerId:
+ *                         type: string
+ *                       anonymousNickname:
+ *                         type: string
+ *                       createdAt:
+ *                         type: object
+ *                         properties:
+ *                           _seconds:
+ *                             type: number
+ *                           _nanoseconds:
+ *                             type: number
+ *                       updatedAt:
+ *                         type: object
+ *                         properties:
+ *                           _seconds:
+ *                             type: number
+ *                           _nanoseconds:
+ *                             type: number
+ *       400:
+ *         description: "클라이언트 측이 잘못된 요청을 보냈습니다."
+ *       500:
+ *         description: "서버에서 내부 오류가 발생했습니다."
+ */
 export async function GET(
   _: unknown,
   { params }: { params: { userId: string } },
