7c1efaad444b8cef5653830b51e06abe7f481eaa
hyodduru <hyodduru@hyottuui-MacBookAir.local>
2024-01-13 15:46:19 +0900

    feat: 대쉬보드 배경 재조정

--- app/dashboard/[userId]/page.tsx
+++ app/dashboard/[userId]/page.tsx
@@ -1,6 +1,7 @@
 import { WriteLetterButton } from 'components/WriteLetterButton';
 import { Header } from 'components/header';
-import DashboardBackground from 'public/images/dashboard-background.png';
+import DashboardGrass from 'public/images/dashboard-grass.png';
+import DashboardTree from 'public/images/dashboard-tree.png';
 
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
@@ -16,18 +17,27 @@ export default async function DashBoard() {
   return (
     <div className="overflow-hidden h-full">
       <Header />
-      <div className="flex justify-center flex-col items-center h-[calc(100vh-48px)]">
-        <img
-          src={DashboardBackground.src}
-          alt="대쉬보드 배경"
-          style={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-          }}
-        />
-        {/* TODO: 임의로 Date */}
-        <CountdownTimer targetDate={new Date('2024-01-10')} />
+      <img
+        src={DashboardTree.src}
+        alt="대쉬보드 배경"
+        style={{
+          position: 'absolute',
+          width: '500px',
+          height: '420px',
+          bottom: '60px',
+        }}
+      />
+      <img
+        src={DashboardGrass.src}
+        alt="대쉬보드 배경"
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+        }}
+      />
+      <div className="flex flex-col h-[calc(100vh-48px)] p-2">
+        <CountdownTimer targetDate={new Date('2024-02-10')} />
         <WriteLetterButton />
         <LetterViewerButton />
       </div>
