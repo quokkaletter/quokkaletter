3250cea5249382ea5528047c5b5dad7fc46119e0
Geuni <dlrmsgnl620@naver.com>
2024-01-30 19:08:11 +0900

    Feat: 메타 데이터 OG 이미지 추가

---
+++ site.config.ts
@@ -0,0 +1,10 @@
+const CONFIG = {
+  title: '쿼카레터',
+  description: '그동안 하지 못했던 말, 새해에 풀어보세요~!',
+  url: 'https://quokkaletter.vercel.app',
+
+  ogImage: '/ogImage.png',
+  favicon: '/favicon.png',
+};
+
+export default CONFIG;
