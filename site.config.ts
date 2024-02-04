0c4c7774d3d673899f373fa32e004ca356f8fef1
hyodduru <hyodduru@hyottuui-MacBookAir.local>
2024-02-04 12:44:26 +0900

    feat: 메타 데이터 추가

--- site.config.ts
+++ site.config.ts
@@ -1,9 +1,9 @@
 const CONFIG = {
-  title: '쿼카레터',
-  description: '그동안 하지 못했던 말, 새해에 풀어보세요~!',
+  title: '쿼카레터로 전하는 따뜻한 진심',
+  description:
+    '그동안 전하지 못한 마음을 쿼카레터로 담아 보내보세요. 함께 시작하는 새해에 상대방에게 닿을 때까지의 설레임과 기대가 담긴 쿼카레터로 더욱 특별한 순간을 만들어보아요!',
   url: 'https://quokkaletter.vercel.app',
-
-  ogImage: '/ogImage.png',
+  ogImage: '/thumbnail.png',
   favicon: '/favicon.png',
 };
 
