63f5dad2f2e78583acbc3cf5bae96d1bd6a1c66a
hyodduru <hyodduru@hyottuui-MacBookAir.local>
2024-02-04 18:53:27 +0900

    fix: 불필요한 링크 공유 성공 메세지를 제거함
    resolves #76

--- components/nav/navitems/LinkClipBoardMenuItem.tsx
+++ components/nav/navitems/LinkClipBoardMenuItem.tsx
@@ -21,8 +21,6 @@ export const LinkClipBoardMenuItem = () => {
         navigator.share({
           text: myLink,
         });
-
-        toast.success('링크가 성공적으로 공유되었습니다.');
       } catch (error) {
         toast.error('다시 한번 시도해보세요!');
       }
